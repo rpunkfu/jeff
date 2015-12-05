ce3a92a4f41dd06685b473b1b7fb141b8e5d4cf7
Oskar Cieslik (rpawfuiml) <oskar.cieslik@gmail.com>
2015-12-05 23:00:51 +0100

    Fix pypi setup

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='jeff',
-    packages=['jeff'],
-    version='0.0.1',
+    version='0.1.1',
     description='Jeff generates license file from the command line.',
     author='Oskar Cieslik',
     author_email='oskar.cieslik@gmail.com',
     url='https://github.com/oskarcieslik/Jeff',
     download_url='https://github.com/oskarcieslik/Jeff',
     keywords=['license', 'github', 'python', 'command line', 'cli'],
-    license='MIT'
+    license='MIT',
+    packages=find_packages(),
+    package_data={
+        'jeff': ['licenses/*']
+    },
+    entry_points={
+        'console_scripts': [
+            'jeff=jeff.jeff:main'
+        ],
+    }
 )
